d41c0691c6ed9fa9ee7c886e7cf0dbdb9a72416d
Daniel Ye <danielyeqt@gmail.com>
2021-06-25 15:17:15 -0400

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+import processing.core.PApplet;
+
+public class Main extends PApplet {
+    private static Main client = null;
+
+    public Main() {
+        if(client != null)
+            throw new RuntimeException("A second client was created when it should've been a singleton!");
+        client = this;
+    }
+
+    public Main main() {
+        if (client == null)
+            client = new Main();
+        return client;
+    }
+
+    public void settings() {
+        size(1000, 800);
+    }
+
+    public void setup() {
+    }
+
+    public void mousePressed() {
+    }
+
+    public void mouseReleased() {
+    }
+
+    public static void main(String[] args) {
+        PApplet.main(new String[]{"Main"});
+    }
+}
